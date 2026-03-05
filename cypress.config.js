43eec7031bf87bd609159002529ae250d262b12f
faiza29feb <37223630+faiza29feb@users.noreply.github.com>
2026-03-05 22:03:05 +0400

    fix: disable missing supportFile warning

--- cypress.config.js
+++ cypress.config.js
@@ -2,10 +2,8 @@ const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
   e2e: {
-    video: false,
-    screenshotOnRunFailure: false,
-    defaultCommandTimeout: 15000,
-    requestTimeout: 15000,
-    responseTimeout: 15000,
-  },
+    supportFile: false,
+    specPattern: "cypress/e2e/**/*.cy.js",
+    video: false
+  }
 });
